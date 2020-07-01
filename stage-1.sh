7d3c351ac5bb6cd61d4304b6881c54119e4d61b8
Tony Arredondo <tony.arredondo5@gmail.com>
2020-07-01 17:22:00 -0500

    update

--- stage-1.sh
+++ stage-1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pacman -Sy --noconfirm pacman-contrib
 curl -s "https://www.archlinux.org/mirrorlist/?country=US&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors - > /etc/pacman.d/mirrorlist
-pacstrap /mnt base base-devel linux linux-firmware intel-ucode xorg lightdm lightdm-webkit2-greeter dialog netctl iw dhcpcd wpa_supplicant inetutils awesome openssh git nano termite ttf-dejavu pulseaudio alsa-utils pacman-contrib acpid pavucontrol udisks2 light
+pacstrap /mnt base base-devel linux linux-firmware intel-ucode xorg lightdm lightdm-webkit2-greeter dialog netctl iw dhcpcd wpa_supplicant inetutils awesome openssh git nano termite ttf-dejavu alsa-utils pacman-contrib acpid pavucontrol udisks2 light
 genfstab -U /mnt > /mnt/etc/fstab
 curl -SsL https://raw.githubusercontent.com/POPCORNrules/arch-setup/master/stage-2.sh -o /mnt/root/stage-2.sh
 chmod +x /mnt/root/stage-2.sh
