1ea32b928be4f6d83224a2502702b9799e2e87bc
Tony Arredondo <tony.arredondo5@gmail.com>
2020-07-04 16:15:42 -0500

    fix yadm clone command

--- stage-3.sh
+++ stage-3.sh
@@ -3,5 +3,5 @@ sudo wifi-menu
 ip link | sudo awk '/wl/{system("systemctl enable netctl-auto@"substr($2, 1, length($2)-1))}'
 if [ "$#" -eq 1 ]
     $DOTFILES=$1
-    yadm clone --bootstrap $DOTFILES
+    yadm clone --bootstrap "$DOTFILES"
 fi
