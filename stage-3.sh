e97515fcee1b7c7f09eb5bbc248816ee72d31f18
Tony Arredondo <tony.arredondo5@gmail.com>
2020-07-18 01:34:53 -0500

    add bluetooth

--- stage-3.sh
+++ stage-3.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sudo wifi-menu
 ip link | sudo awk '/wl/{system("systemctl enable netctl-auto@"substr($2, 1, length($2)-1))}'
+sudo systemctl start bluetooth
+sudo systemctl enable bluetooth
 if [ "$#" -eq 1 ]
     $DOTFILES=$1
     yadm clone --bootstrap "$DOTFILES"
